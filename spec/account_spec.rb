c103fc12179ae82ac86fca4860a710a9707432e2
Matthew Lock <matthew_lock20@hotmail.com>
2020-08-10 16:15:17 +0100

    Created withdraw method along with passing test

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,17 +1,23 @@
 require './lib/account'
 
 describe Account do
+  subject(:account) { Account.new }
   describe 'Create an account' do
     it 'creates a user account that has a zero balance' do
-      account = Account.new
       expect(subject.balance).to eq(0)
     end
   end
   describe 'Deposit funds into account' do
     it 'allows user to deposit funds into account' do
-      account = Account.new
-      account.deposit(500)
-      expect(account.balance).to eq(500)
+      subject.deposit(500)
+      expect(subject.balance).to eq(500)
+    end
+  end
+  describe 'Withdraw funds from account' do
+    it 'allows user to withdraw funds from their account' do
+      subject.deposit(1000)
+      subject.withdraw(500)
+      expect(subject.balance).to eq(500)
     end
   end
 end
