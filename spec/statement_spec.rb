09b172f7ffdc836463a72a1d7918b8f24d0e97f9
Matthew Lock <matthew_lock20@hotmail.com>
2020-08-12 17:50:48 +0100

    simplecov gem added, 100% test coverage, all tests passing

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -1,13 +1,15 @@
 require './lib/statement'
+require './lib/account'
 
 describe Statement do
   describe 'Expects print to receive 1 argument' do
       statement = Statement.new
       it { is_expected.to respond_to(:print).with(1).argument }
   end
-    xit'prints the statement for the user' do
-      subject.deposit('10-01-2012'  , 1000)
-      subject.withdraw('12-01-2012', 250)
+    it'prints the statement for the user' do
+      account = Account.new
+      account.deposit('10-01-2012'  , 1000)
+      account.withdraw('12-01-2012', 250)
       statement = <<~STATEMENT
         date || credit || debit || balance
         12-01-2012 || || 250.00 || 750.00
