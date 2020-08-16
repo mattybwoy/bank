dada993a431db1169fbf1a0401023be1b883a842
Matthew Lock <matthew_lock20@hotmail.com>
2020-08-16 10:21:11 +0100

    Using mock for statement class, not yet displaying correct output

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -2,19 +2,21 @@ require './lib/statement'
 require './lib/account'
 
 describe Statement do
+  # let(:account) {double :account}
+  subject(:statement) { Statement.new }
+  let(:transaction) { double :transaction, date: 10/1/2012, input: 1000, balance: 1000 }
+  let(:transactions) {[transaction]}
+
   describe 'Expects print to receive 1 argument' do
       statement = Statement.new
       it { is_expected.to respond_to(:print).with(1).argument }
   end
+
     it'prints the statement for the user' do
-      account = Account.new
-      account.deposit('10-01-2012'  , 1000)
-      account.withdraw('12-01-2012', 250)
       statement = <<~STATEMENT
         date || credit || debit || balance
-        12/1/2012 || || 250.00 || 750.00
         10/1/2012 || 1000.00 || || 1000.00
       STATEMENT
-      expect { account.print_statement }.to output(statement).to_stdout
+      expect { subject.print(transactions) }.to output(statement).to_stdout
     end
   end
